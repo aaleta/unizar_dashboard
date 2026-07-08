71afa18d7dfaefc89031031f0843b9cbdbc2c304
Marcos9001 <lizanomarcos901@gmail.com>
2026-07-08 15:45:28 +0200

    npm run build done

--- web/vite.config.js
+++ web/vite.config.js
@@ -4,15 +4,17 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
-// https://vite.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [
     vue(),
-    vueDevTools(),
-  ],
+    mode === 'development' && vueDevTools(),
+  ].filter(Boolean),
+
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
-})
+
+  base: '/unizar_dashboard.git/',
+}))
