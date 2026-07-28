6c6625e02f9082482c2ebadeae012a11cbc8ba05
aleta <albertoaleta@gmail.com>
2026-07-29 00:17:02 +0200

    Updates

--- assets/UiPill-74J7Hspz.js
+++
@@ -1,1 +0,0 @@
-import{U as e,bt as t,dt as n,lt as r,tt as i}from"./index-BNB7VcAb.js";var a=e({__name:`UiPill`,props:{tone:{type:String,default:`navy`,validator:e=>[`navy`,`neutral`,`gold`].includes(e)}},setup(e){return(a,o)=>(r(),i(`span`,{class:t([`pill`,e.tone])},[n(a.$slots,`default`,{},void 0,!0)],2))}},[[`__scopeId`,`data-v-8c86293c`]]);export{a as t};
